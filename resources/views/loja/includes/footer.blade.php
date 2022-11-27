8f53baccbb9e006ba3e0380f497d2faa876b0c0f
hsilvaMR <zagaftuga@gmail.com>
2022-11-27 17:34:00 +0100

    update files header | footer

--- resources/views/loja/includes/footer.blade.php
+++ resources/views/loja/includes/footer.blade.php
@@ -1,13 +1,110 @@
-<footer class="footer-loja">
+<!-- Footer -->
+<footer class="text-center text-lg-start  footer-loja  text-muted">
 
-    <div class="container">
+    <!-- Section: Links  -->
+    <section class="mt-3" >
+        <div class="container border-bottom border-2 text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3 text-secondary"></i>Company name
+                    </h6>
+                    <p>
+                        Subscreva a nossa newsletter para ficar a par de todas as novidades promoções e muito mais
+                    </p>
+                    {{-- formulario newsletter --}}
+                    <div class="d-flex justify-content-start align-items-center box-subscrever">
+                        {{-- field text --}}
+                        <input type="text" id="txtPesquisa" placeholder="EX. mail@domain.com" aria-required="true">
+                        {{-- botao pesquisa --}}
+                        <div class="p-2 btn-subscrever">
+                            <span>SUBSCREVER</span> 
+                        </div>
+                    </div>
+                </div>
+                <!-- Grid column -->
 
-        <div class="row">
-            <div class="col-sm-8">col-sm-8</div>
-            <div class="col-sm-4">col-sm-4</div>
-          </div>
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!--  Links úteis  -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Links úteis 
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">SOBRE NÓS</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">DEVOLUÇÕES</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">POLITICA DE PRIVACIDADE</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">PARCEIROS</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">LIVRO DE RECLAMAÇÃO</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">PAGAMENTO E SEGURANÇA</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
 
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Categorias
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">TELEMÓVEL</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">COMPUTADOR</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">TV & VÍDEO</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">TABLET</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">SMART WATCH</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">ACESSÓRIO DE INFORMÁTICA</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+                    <p><i class="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+                    <p>
+                        <i class="fas fa-envelope me-3 text-secondary"></i>
+                        info@hesashop.com
+                    </p>
+                    <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
-
-
+    <!-- Copyright -->
 </footer>
+<!-- Footer -->

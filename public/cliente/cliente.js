a1e3436999ae08d7b1b98fafeaa20063399e1d45
hsilvaMR <zagaftuga@gmail.com>
2023-01-14 13:57:58 +0100

    update cliente area

---
+++ public/cliente/cliente.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $('#showMenu').on('click', function() {
+        $('.dropDownMenu').removeClass("d-none")
+        $('#showMenu').addClass("d-none")
+        $('#hideMenu').removeClass("d-none")
+    })
+
+})
